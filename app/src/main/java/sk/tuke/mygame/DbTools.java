622989b2df75371d3778cdc71030face258bf9bf
Marek Puškáš <113102926+Marek243-sk@users.noreply.github.com>
2024-01-25 14:28:21 +0100

    komentáre

--- app/src/main/java/sk/tuke/mygame/DbTools.java
+++ app/src/main/java/sk/tuke/mygame/DbTools.java
@@ -8,15 +8,19 @@ import java.lang.ref.WeakReference;
 
 public class DbTools {
 
+//    Inštancia triedy
     private static GameDatabase _db;
     public DbTools(WeakReference<Context> refContext) {
+//        Inicializácia db, pri vytvorení inštancie triedy
         getDbContext(refContext);
     }
-
+//    Metóda pre získanie inštancie db
     static GameDatabase getDbContext(WeakReference<Context> refContext) {
+//        Ak je db inicializovana, vráť existujúcu inštanciu
         if (_db != null) {
             return _db;
         }
+//        Vytvorenie novej db/ načítanie existujúcej pomocou Room knižnice
         return Room.databaseBuilder(refContext.get(),GameDatabase.class, "game-db").build();
     }
 }
--- app/src/main/java/sk/tuke/mygame/Game.java
+++ app/src/main/java/sk/tuke/mygame/Game.java
@@ -4,23 +4,24 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+//    Tabuľka 'Game'
 @Entity
 public class Game {
-
+//    Definovanie PK (automaticky generovaný) a stĺpca
     @PrimaryKey(autoGenerate = true)
     private long id;
 
     @ColumnInfo(name = "points")
     private int points;
 
-
+//    Prázdny konštruktor, Room ho vyžaduje.
     public Game() {
     }
-
+//    Konštruktor, inicializuje premennú na uchovanie bodov v hre (jednej)
     public Game(int points) {
         setPoints(points);
     }
-
+//    Getter → získanie, Setter → nastavenie
     public long getId() {
         return id;
     }
